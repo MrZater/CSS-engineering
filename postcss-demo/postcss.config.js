4b3c36b455da2b1f9a0252ad2487f141d99186d9
zt <zhoutao@ydmob.com>
2024-05-20 14:44:58 +0800

    class2-4

--- postcss-demo/postcss.config.js
+++ postcss-demo/postcss.config.js
@@ -1,32 +1,34 @@
-const styleLint = require("stylelint");
-const autoprefixer = require("autoprefixer");
-const cssnanoPlugin = require("cssnano");
 
 /*
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-05-17 19:24:36
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-05-20 12:01:25
+ * @LastEditTime: 2024-05-20 14:42:30
  * @FilePath: /CSS-engineering/postcss-demo/postcss.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+const styleLint = require("stylelint");
+const autoprefixer = require("autoprefixer");
+const cssnanoPlugin = require("cssnano");
+const PostPresetEnv =require('postcss-preset-env')
+const Import = require("postcss-import");
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
-        styleLint({
-            fix: true
+        Import({
+            path: "./src/css",
+            // 被合并到 index.css 之前，会被 postcssNested 这个插件先处理一遍
+            plugins: [
+                // postcssNested
+            ]
         }),
-        autoprefixer,
-        cssnanoPlugin({
-            // preset: [
-            //     "default",
-            //     {
-            //         // 保留单行注释
-            //         discardComments: false,
-
-            //         // 保留空样式
-            //         discardEmpty: false
-            //     }
-            // ]
+        PostPresetEnv({
+            stage: 2
+        }),
+        purgecss({
+            content: ['./src/*.html'],
+            safelist: [/-active/, /-focus/, /-hover/]  
         })
     ],
     
