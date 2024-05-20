65f13f46183627e973d32c34735b75e23eba6df4
zt <zhoutao@ydmob.com>
2024-05-20 15:28:01 +0800

    class2-5

--- postcss-demo/postcss.config.js
+++ postcss-demo/postcss.config.js
@@ -3,7 +3,7 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-05-17 19:24:36
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-05-20 14:42:30
+ * @LastEditTime: 2024-05-20 14:47:02
  * @FilePath: /CSS-engineering/postcss-demo/postcss.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,20 +16,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
     plugins: [
-        Import({
-            path: "./src/css",
-            // 被合并到 index.css 之前，会被 postcssNested 这个插件先处理一遍
-            plugins: [
-                // postcssNested
-            ]
-        }),
-        PostPresetEnv({
-            stage: 2
-        }),
-        purgecss({
-            content: ['./src/*.html'],
-            safelist: [/-active/, /-focus/, /-hover/]  
-        })
     ],
     
     
