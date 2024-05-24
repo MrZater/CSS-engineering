b6858b5df4593949adbc3c983c8633062861c7b8
zt <zhoutao@ydmob.com>
2024-05-24 17:05:42 +0800

    class3-8

--- tailwindcss-demo/tailwind.config.js
+++ tailwindcss-demo/tailwind.config.js
@@ -2,7 +2,7 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-05-20 18:25:06
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-05-24 15:01:38
+ * @LastEditTime: 2024-05-24 17:02:41
  * @FilePath: /CSS-engineering/tailwindcss-demo/tailwind.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,9 +16,13 @@ module.exports = {
     },
   },
   variants: {
-    extends: {
+    extend: {
+      opacity: ["btn-hover"]
     }
   },
   plugins: [],
-  darkMode: 'class'
+  darkMode: 'class',
+  safelist: [
+    'bg-red-100',
+  ]
 }
